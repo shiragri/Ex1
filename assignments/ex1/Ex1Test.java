cfa850a7ab0b244a7c7a74ab4c6897f6a19363a6
shiragri <shiragrinvald@gmail.com>
2024-12-08 20:30:57 +0200

    Test

--- assignments/ex1/Ex1Test.java
+++ assignments/ex1/Ex1Test.java
@@ -28,7 +28,7 @@ public class Ex1Test {
                 boolean ok = Ex1.isNumber(good[i]);
                 assertTrue(ok);
             }
-            String[] not_good = {"b2", "2b2", "1G3bG", " BbG", "0bbA", "abB", "!@b2", "A", "1bb2"};
+            String[] not_good = {"b2", "2b2", "1G3bG", " BbG", "0bbA", "abB", "!@b2", "A"," 4b8", "b12","1bb2"};
             for(int i=0;i<not_good.length;i=i+1) {
                 boolean not_ok = Ex1.isNumber(not_good[i]);
                 assertFalse(not_ok);
@@ -51,9 +51,25 @@ public class Ex1Test {
 
 
         }
+        @Test
+        void bacetointTest() {
+            char s= 'G';
+            int v= Ex1.baseToInt(s);
+            assertEquals(v,16);
+            s= '9';
+            v= Ex1.baseToInt(s);
+            assertEquals(v,9);
+            s= 'A';
+            v= Ex1.baseToInt(s);
+            assertEquals(v,10);
+            s= 'a';
+            v= Ex1.baseToInt(s);
+            assertEquals(v,-1);
+
+        }
 
         @Test
-        void number2Int() {
+        void number2IntTest() {
             String s2 = "1011b2";
             int v = Ex1.number2Int(s2);
             assertEquals(v,11);
